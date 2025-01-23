f3b1cc08d9b4ce9015261feedd75a6cb1a8440ec
Mika Hyttinen <mika.hyttinen@gmail.com>
2025-01-23 17:41:06 +0200

    Fix some typos

--- CMI.sh
+++ CMI.sh
@@ -33,16 +33,16 @@ Help()
 
 cat << EOF
 
-    Welcome to Cellframe Mastenode installation script.  
+    Welcome to Cellframe Masternode installation script.
 
-    This script will install Cellframe Node to your computer and configure a master role in selected network. 
+    This script will install Cellframe Node to your computer and configure a master role in selected network.
 
-    Cellframe Mastenode installation script will attempt to locate a .dwallet file in the same directory where it is located. 
-    We recommend to use this option and copy the wallet file prior to launching the masternode configuration. 
+    Cellframe Masternode installation script will attempt to locate a .dwallet file in the same directory where it is located.
+    We recommend to use this option and copy the wallet file prior to launching the masternode configuration.
 
     Alternatively, seed phrase from your wallet (24 words + password) can be used to restore the wallet that was created in Cellframe Dashboard.
 
-    How to begin masternode configuration? 
+    How to begin masternode configuration?
 
     Run CMI.sh script with a desired network as a parameter. If network name is not provided, Backbone will be selected by default.
 
@@ -50,7 +50,7 @@ cat << EOF
         bash CMI.sh -n Backbone
         bash CMI.sh -n KelVPN
 
-    Additionally, an option to provide a link to cellframe-node .deb file is available. 
+    Additionally, an option to provide a link to cellframe-node .deb file is available.
     If not provided, latest available release will be used.
 
     Example:
@@ -58,19 +58,19 @@ cat << EOF
 
     In case any issues are encountered during the masternode configuration, contact the original author of the script or Cellframe Support team.
 
-    Originally created by hyttmi  
-    @CELLgainz in Telegram 
+    Originally created by hyttmi
+    @CELLgainz in Telegram
 
     Cellframe Support group in Telegram: https://t.me/cellframetechsupport
     Cellframe Support e-mail address: tech_support@demlabs.net
 
 EOF
 
-exit 
+exit
 }
 
 
-## parsing command-line options and arguments 
+## parsing command-line options and arguments
 
 optstring=n:l:h
 
@@ -133,7 +133,7 @@ restart_node ()
 
 showinfo() {
 
-    printf "%s Cellframe Mastenode installation script launched.\n" "---"
+    printf "%s Cellframe Masternode installation script launched.\n" "---"
 
     read -p "--- Are you sure you want to continue? (Y/N) " confirm
     if [[ $confirm =~ ^[yY]$ ]]; then
@@ -154,10 +154,10 @@ check_node_presence() {
     check_deps
 }
 
-check_deps() 
+check_deps()
 {
     echo "--- Checking necessary dependencies..."
-    DEPS=( sed apt wget sha256sum wc pv irqbalance dig ) 
+    DEPS=( sed apt wget sha256sum wc pv irqbalance dig )
     for i in ${DEPS[@]}
     do
         if [[ $(command -v $i) ]]; then
@@ -175,23 +175,23 @@ check_deps()
 check_if_link_provided()
 {
 if [[ -z ${NODE_DEB_LINK} ]];
-then 
+then
     printf "Link to the cellframe-node package is not provided. Latest release from master branch will be used.\n"
     download_latest
 elif [[ -n ${NODE_DEB_LINK} ]];
 then
     printf "Link to the cellframe-node package is provided.\n"
     check_if_correct_link ${NODE_DEB_LINK}
-fi 
+fi
 }
 
 check_if_correct_link()
 {
 
-if [[ $1 =~ https:\/\/pub\.cellframe\.net\/linux\/cellframe-node\/* ]]; 
+if [[ $1 =~ https:\/\/pub\.cellframe\.net\/linux\/cellframe-node\/* ]];
 then
   wget --spider -q $1
-  if [ $? -eq 0 ]; 
+  if [ $? -eq 0 ];
   then
     printf "Correct link provided, file exists.\n"
     check_correct_arch_in_link
@@ -199,10 +199,10 @@ then
     printf "Error! File doesn't exist! Latest release from master branch will be used.\n"
     download_latest
   fi
-else 
+else
     printf "Incorrect link provided. Latest release from master branch will be used.\n"
     download_latest
-fi 
+fi
 }
 
 check_correct_arch_in_link()
@@ -211,13 +211,13 @@ _temp=${NODE_DEB_LINK##*-}
 LINK_ARCH=${_temp%.*}
 
 if [[ ${ARCH} == ${LINK_ARCH} ]];
-then 
+then
     printf "Link with compatible node architecture provided. Continuing...\n"
     download_from_link
 else
     printf "Link with incompatible node architecture provided. Latest compatible release from master branch will be used.\n"
     download_latest
-fi 
+fi
 }
 
 
@@ -228,7 +228,7 @@ ARCH=$(dpkg --print-architecture)
 NODE_VERSION=$(curl -v --silent https://pub.cellframe.net/linux/cellframe-node/master/ 2>&1 | grep -oP ">cellframe-node-\K5.3.[0-9]{3}" | sort | tail -n1)
 printf "Latest available cellframe-node version on pub.cellframe.net: %s\n" "${NODE_VERSION}"
 
-case "$ARCH" in 
+case "$ARCH" in
   amd64) LATEST_FILE_NAME=$(wget -qO- https://pub.cellframe.net/linux/cellframe-node/master/ | grep -oP "\Kcellframe-node-5.3.[0-9]{3}-rwd-amd64.deb" | sort | tail -n1)
   ;;
   arm64) LATEST_FILE_NAME=$(wget -qO- https://pub.cellframe.net/linux/cellframe-node/master/ | grep -oP "\Kcellframe-node-5.3.[0-9]{3}-arm64.deb" | sort | tail -n1)
@@ -272,39 +272,39 @@ NET_LIST=( Backbone KelVPN raiden riemann )
 if echo ${NET_LIST[@]^^} | grep -qw ${NET_NAME^^};      # check for valid netname
 then
     for i in "${NET_LIST[@]}"
-    do 
+    do
         if [[ ${NET_NAME^^} == ${i^^} ]];
-        then 
+        then
             NET_NAME=${i}       # overwrite NET_NAME in case input was not case-sensitive
         fi
-    done  
+    done
 else
     NET_NAME=Backbone
     printf "Provided unknown network name. Proceeding with masternode configuration for Backbone network.\n"
 fi
 
 
-if [[ -z ${NET_NAME} ]];    ## check if no netname input 
-then 
+if [[ -z ${NET_NAME} ]];    ## check if no netname input
+then
     NET_NAME=Backbone
     printf "Network name is not provided. Proceeding with masternode configuration for Backbone network.\n"
-fi 
+fi
 
 printf "Configuring cellframe-node networks.\n" | tee -a $LOG
 for _net in "${NET_LIST[@]}"
-do 
+do
     if [[ ${NET_NAME^^} == ${_net^^} ]];
-    then 
+    then
         _state=on
-    else 
+    else
         _state=off
     fi
     printf "Turning %s %s network.\n" "${_state}" "${_net}" | tee -a $LOG
     /opt/cellframe-node/bin/cellframe-node-config -e network ${_net} ensure ${_state}
     printf "\n"
-done 
+done
 
-restart_node 
+restart_node
 verify_node_running
 create_cert
 }
@@ -314,7 +314,7 @@ create_cert() {
     cert=${cert:-master}
         if [[ $cert =~ ^[a-zA-Z0-9]*$ ]]; then
             echo "--- Your certificate ${NET_NAME}.${cert} will be created..."
-            /opt/cellframe-node/bin/cellframe-node-tool cert create ${NET_NAME}.${cert} sig_dil | tee -a $LOG 
+            /opt/cellframe-node/bin/cellframe-node-tool cert create ${NET_NAME}.${cert} sig_dil | tee -a $LOG
             declare -x -g CERT="${NET_NAME}.${cert}"
             check_wallet_files
         else
@@ -337,7 +337,7 @@ check_wallet_files() {
             done
             read -p "--- Which one would you like to restore?: " number
             MAX_VALUE=$(($ARRAY_LEN - 1))
-            if [[ $number =~ ^[0-9]$ && ! $number -gt $MAX_VALUE ]]; then 
+            if [[ $number =~ ^[0-9]$ && ! $number -gt $MAX_VALUE ]]; then
                 cp "$SCRIPT_DIR/${WALLETFILES[$number]}" $WALLETPATH
                 echo "--- Wallet ${WALLETFILES[$number]} restored!"
                 walletname="${WALLETFILES[$number]%.*}"
@@ -369,18 +369,18 @@ check_wallet_files() {
 check_wallet_password()
 {
 if [[ $( run_cli wallet info -w ${WALLETNAME} -net ${NET_NAME} | grep error ) ]];
-then 
+then
     printf "%s Wallet is password protected.\n" "---"
     read -p "--- Enter wallet password to unlock it: " _password
     until [[ $( run_cli wallet activate -w ${WALLETNAME} -password ${_password} | grep activated ) ]];
-    do 
+    do
         read -p "--- Incorrect wallet password. Try again: " _password
-    done 
+    done
     printf "%s Wallet activated.\n" "---"
 elif [[ $( run_cli wallet info -w ${WALLETNAME} -net ${NET_NAME} | grep addr ) ]];
-then 
+then
     printf "%s Wallet is not password protected.\n" "---"
-fi 
+fi
 }
 
 
@@ -396,20 +396,20 @@ create_wallet() {
         fi
 }
 
-get_seed() 
+get_seed()
 {
     get_sign
-    
+
     read -p "--- Is your wallet password protected? (Y/N) " _if_password
 
-    if [[ ${_if_password} =~ ^[yY]$ ]]; 
+    if [[ ${_if_password} =~ ^[yY]$ ]];
     then
         _word_num=25
         _message="Input your 24 word seed phrase plus a password as 25th word!"
     else
         _word_num=24
         _message="Input your 24 word seed phrase."
-    fi 
+    fi
 
     printf "%s %s\n" "---" "${_message}"
     read -p "---> " words
@@ -445,14 +445,14 @@ done
 
 read -p "---> Enter number: " number
 
-if [[ $number =~ ^[0-9]$ && ! $number -gt $_MAX_VALUE ]]; 
+if [[ $number =~ ^[0-9]$ && ! $number -gt $_MAX_VALUE ]];
 then
     declare -x -g _sign_type=${_signs_available[${number}]}
     printf "%s %s signature selected.\n" "---" "${_sign_type}" | tee -a $LOG
-else 
+else
     printf "%s Not a valid number!\n\n" "---"
-    get_sign 
-fi 
+    get_sign
+fi
 }
 
 configure_node() {
@@ -465,13 +465,13 @@ configure_node() {
 
     echo "--- Modifying ${NET_NAME} configuration..."
 
-    # deleting template lines 
+    # deleting template lines
     sed -i '/esbocs/d' ${NET_CONFIG_FILE}
     sed -i '/consensus_debug/d' ${NET_CONFIG_FILE}
     sed -i '/blocks-sign-cert/d' ${NET_CONFIG_FILE}
     sed -i '/collecting_level/d' ${NET_CONFIG_FILE}
     sed -i '/fee_addr/d' ${NET_CONFIG_FILE}
-    
+
     # entering new configuration
     sed -i "s/^node-role=.*/node-role=${NODE_ROLE}/g" $NET_CONFIG_FILE
     echo "[esbocs]" >> $NET_CONFIG_FILE
@@ -495,19 +495,19 @@ configure_node() {
 check_net_state()
 {
 _not_ready=( NET_STATE_LINKS_PREPARE NET_STATE_LINKS_CONNECTING NET_STATE_LOADING NET_STATE_LINKS_ESTABLISHED )
-_current_state=$( run_cli net -net ${NET_NAME} get status | grep -A1 states | grep current | cut -d: -f2 | tr -d ' ' ) 
+_current_state=$( run_cli net -net ${NET_NAME} get status | grep -A1 states | grep current | cut -d: -f2 | tr -d ' ' )
 
 while true;
-do 
+do
     if [[ ${_not_ready[@]} =~ ${_current_state} ]];
-    then 
+    then
         printf "Cellframe Node is loading. Current network state: %s\n" "${_current_state}"
         sleep 10
-        _current_state=$( run_cli net -net ${NET_NAME} get status | grep -A1 states | grep current | cut -d: -f2 | tr -d ' ' ) 
-    else 
-        break 
+        _current_state=$( run_cli net -net ${NET_NAME} get status | grep -A1 states | grep current | cut -d: -f2 | tr -d ' ' )
+    else
+        break
     fi
-done 
+done
 
 printf "Cellframe Node loaded. Current network state: %s\n\n" "${_current_state}"
 }
@@ -526,7 +526,7 @@ sync_progress()
 check_chain_sync zerochain
 
 until [[ ${_sync_progress} -eq 100 ]]
-do 
+do
     printf "%s synchronization in progress.\n" "${_chain^}"
     printf "Progress: %s %s\n" "${_sync_progress}" "%"
     sleep 30
@@ -537,10 +537,10 @@ printf "%s is fully synchronized at 100 %s\n\n" "${_chain^}" "%" | tee -a $LOG
 
 sleep 5
 
-check_chain_sync main 
+check_chain_sync main
 
 until [[ ${_sync_progress} -eq 100 ]]
-do 
+do
     printf "%s synchronization in progress.\n" "${_chain^}"
     printf "Progress: %s %s\n" "${_sync_progress}" "%"
     sleep 10
@@ -556,24 +556,24 @@ publish_node
 publish_node()
 {
 for _count in {1..10}
-do 
+do
     echo "--- Publishing node...."
-    publish_result=$( run_cli node add -net ${NET_NAME} ) 
+    publish_result=$( run_cli node add -net ${NET_NAME} )
     sleep 5
 
-    if [[ $( echo ${publish_result} | grep "error" ) ]]; 
+    if [[ $( echo ${publish_result} | grep "error" ) ]];
     then
-        publish_check=$( run_cli node list -net ${NET_NAME} ) 
+        publish_check=$( run_cli node list -net ${NET_NAME} )
 
         if [[ $( echo ${publish_check} | grep "${NODE_ADDR}" ) ]];
-        then 
+        then
             printf "\tNode successfully added to %s node list.\n\n" "${NET_NAME}" | tee -a $LOG
             create_diag_data
         else
             echo "--- Attempt failed. Trying again in 10 seconds."
             sleep 10
         fi
-        
+
     elif [[ $( echo ${publish_result}  | grep "Successfully" ) ]];
     then
         printf "\tNode successfully added to %s node list.\n\n" "${NET_NAME}" | tee -a $LOG
@@ -585,10 +585,10 @@ do
         printf "Exiting script...\n"
         exit 1
     fi
-done 
+done
 
 printf "Not able to publish node. Are you behind a firewall/NAT? Check if port 8079 is open.\n" | tee -a $LOG
-printf "Exiting script...\n" 
+printf "Exiting script...\n"
 exit 1
 }
 
@@ -610,17 +610,17 @@ check_token_balance() {
     token=$1
 
     if [[ ${token} = m* ]];
-    then 
+    then
         _accept_value=$( run_cli srv_stake list keys -net ${NET_NAME} | grep -w key_delegating_min_value | cut -d: -f2 | cut -d. -f1 )
         _message="Minimum required amount to start masternode:"
-    else 
+    else
         _accept_value='1'
         _message="Recommended amount for operations:"
     fi
 
     printf "%s Checking your %s balance...\n" "---" "${token}"
     BALANCE=$(run_cli wallet info -w ${WALLETNAME} -net ${NET_NAME} | grep -B3 -w ${token} | grep coins | cut -d: -f2 | cut -d. -f1)
-    if [[ ${BALANCE} -lt ${_accept_value} ]]; 
+    if [[ ${BALANCE} -lt ${_accept_value} ]];
     then
         printf "%s Looks like you don't have enough %s on your wallet.\n" "---" "${token}" | tee -a $LOG
         printf "%s Your balance: %s %s\n" "---" "${BALANCE:-0}" "${token}"
@@ -638,13 +638,13 @@ lock_mtoken() {
     token=${mtoken_array[${NET_NAME}]}
     BALANCE=$(run_cli wallet info -w ${WALLETNAME} -net ${NET_NAME} | grep -B3 -w ${token} | grep coins | cut -d: -f2 | cut -d. -f1)
     printf "%s Your current wallet balance is %s %s \n" "---" ${BALANCE} ${mtoken_array[${NET_NAME}]}
-    read -p "Enter the amount which you want to lock for your mastenode (no decimals): " mtoken_amount
-    if [[ ! ${mtoken_amount} =~ ^[0-9]*$ ]]; 
+    read -p "Enter the amount which you want to lock for your masternode (no decimals): " mtoken_amount
+    if [[ ! ${mtoken_amount} =~ ^[0-9]*$ ]];
     then
         echo "--- Invalid amount of tokens! Please try again..."
         lock_mtoken
     elif [[ ${mtoken_amount} -gt ${BALANCE} ]];
-    then 
+    then
         printf "%s Entered amount is greater than available %s on wallet! Please try again...\n" "---" "${token}"
         lock_mtoken
     else
@@ -652,16 +652,16 @@ lock_mtoken() {
         delegate_result=$( run_cli srv_stake delegate -cert $CERT -net ${NET_NAME} -w ${WALLETNAME} -value ${mtoken_amount}.0e+18 -fee 0.05e+18 )
 
         if [[ $( echo ${delegate_result} | grep "success" ) ]];
-        then 
+        then
             delegate_hash=$( printf "${delegate_result}\n" | grep tx_hash | cut -d: -f2 )
             printf "Transaction created successfully. Tx hash: %s\n" "${delegate_hash}" | tee -a $LOG
             msg_ready
         elif [[ $( echo ${delegate_result} | grep "error" ) ]];
-        then 
+        then
             printf "Something went wrong. Transaction was not created.\n" | tee -a $LOG
             printf "${delegate_result}\n" | tee -a $LOG
             exit 1
-        fi 
+        fi
     fi
 }
 
@@ -670,7 +670,7 @@ msg_ready()
 printf "\nCongratulations, masternode in %s network is successfully configured!\n" "${NET_NAME}"
 printf "Please contact Cellframe Support team and send your transaction hash for masternode approval.\n"
 printf "Hash of transaction that locked %s %s: %s\n" "${mtoken_amount}" "${mtoken_array[${NET_NAME}]}" "${delegate_hash}"
-printf "Cellframe Support group in Telegram: %s\n" "https://t.me/cellframetechsupport" 
+printf "Cellframe Support group in Telegram: %s\n" "https://t.me/cellframetechsupport"
 printf "Cellframe Support e-mail address: %s\n" "tech_support@demlabs.net"
 
 printf "\nAfter receiving approval from Cellframe Support team, two more things need to be done for the proper masternode functioning.\n"
@@ -688,7 +688,7 @@ else
     exit
 fi
 
-exit 
+exit
 }
 
 
